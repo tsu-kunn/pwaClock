1d3262ae5ad177858727116d07a973a8203fe646
Tsuyoshi.A <perilla.k@gmail.com>
2021-06-26 23:36:29 +0900

    Add debug information

--- src/store/store.js
+++ src/store/store.js
@@ -46,7 +46,10 @@ const reducer = (state = initialState, action) => {
     }
 };
 
-const store = createStore(reducer);
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 export default store;
